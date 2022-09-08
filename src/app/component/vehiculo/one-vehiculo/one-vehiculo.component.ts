9e1106249fd72a5d21e09be1dc64a8c4e63233e3
Your Name <youremail@yourdomain.com>
2022-09-08 00:58:56 -0500

    feat: add get more detail vehiculo, user information

--- src/app/component/vehiculo/one-vehiculo/one-vehiculo.component.ts
+++ src/app/component/vehiculo/one-vehiculo/one-vehiculo.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Marca } from 'src/app/models/marca';
+import { Usuario } from 'src/app/models/usuario';
 import { Vehiculo } from 'src/app/models/vehiculo';
+import { MarcaService } from 'src/app/services/marca.service';
+import { UsuarioService } from 'src/app/services/usuario.service';
 import { VehiculoService } from 'src/app/services/vehiculo.service';
 
 @Component({
@@ -9,12 +13,19 @@ import { VehiculoService } from 'src/app/services/vehiculo.service';
 })
 export class OneVehiculoComponent implements OnInit {
   vehiculo: Vehiculo | any ;
+  marca:Marca |any;
+  usuario:Usuario | any;
   letra:string='perro'
-  constructor(private vehiculoService:VehiculoService) { }
+  constructor(private vehiculoService:VehiculoService,private marcaService:MarcaService,private usuarioService:UsuarioService) { }
 
   ngOnInit(): void {
 this.getVehiculo()
+this.getMarcaByID()
+this.getUsuaioByID()
   }
+/**
+ * * esta funcion obtien un vehiculo por id
+ */
   getVehiculo(): void{
     this.vehiculoService.getOneVehiculo()
     .subscribe({
@@ -25,4 +36,30 @@ this.getVehiculo()
       }
      })
   }
+
+/***
+ * * esta funcion obtiene la marca del vehiculo
+ */
+   getMarcaByID(): void{
+   this.marcaService.getOneMarca()
+    .subscribe({
+      next:(response:  Marca| any) =>{
+        this.marca=response[0]
+       
+      }
+     })
+  }
+/***
+ * * esta funcion obtiene el usuario o duenio del vehiculo
+ */
+   getUsuaioByID(): void{
+   this.usuarioService.getOneUsuario()
+    .subscribe({
+      next:(response:  Usuario| any) =>{
+        this.usuario=response[0]
+       
+      }
+     })
+  }
+  
 }
